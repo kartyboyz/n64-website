ca9314a2d59758f8b26948512102365320c52ec6
Jonathan Bell <jbell@bu.edu>
2014-03-23 04:12:19 -0400

    initial commit, finally putting online

---
+++ n64/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from n64.views import home, query, current_datetime, hours_ahead
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',url(r'^$', home), 
+			  url(r'^query/$', query),	
+			  url(r'^time/$', current_datetime),	
+			  url(r'^time/(\d{1,2})/$', hours_ahead),	
+                          url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+)
+    # Examples:
+    # url(r'^n64/', include('n64.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
---
+++ n64/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
+
+def home(request):
+    return render(request, 'base.html')
+
+def query(request):
+    return render(request, 'query.html')
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    return render(request, 'current_datetime.html', {'current_date': now})
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
