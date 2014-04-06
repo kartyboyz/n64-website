045f33307ee934b9a5574c04eb7f73c1aa8ac07d
Jonathan Bell <jbell@bu.edu>
2014-04-06 17:39:59 -0400

    added forms to be passed to michaels server

--- n64/urls.py
+++ n64/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from n64.forms import QueryForm
-from n64.views import home, query, watch, upload, login, current_datetime, hours_ahead
+from n64.views import home, query, watch, upload
+from django.contrib.auth.views import login, logout
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -12,13 +14,8 @@ urlpatterns = patterns('',
 			  url(r'^watch/$', watch),	
 			  url(r'^upload/$', upload),	
 			  url(r'^login/$', login),	
-			  url(r'^time/$', current_datetime),	
-			  url(r'^time/(\d{1,2})/$', hours_ahead),	
-                          url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+			  url(r'^logout/$', logout),	
 )
-    # Examples:
-    # url(r'^n64/', include('n64.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
--- n64/views.py
+++ n64/views.py
@@ -1,39 +1,50 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
+import base64, json, urllib, hmac, time, hashlib
+import uuid, os, datetime
+
 from n64.forms import QueryForm
 
 def home(request):
     return render(request, 'base.html')
 
 def query(request):
+    ##if we have a query, send query, wait for response
     if request.method == 'post':
         form = QueryForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            ##do stuff with data
+            ##new query post to db
+            query_string = cd['query']
+            query = json.dumps({'query': query_string})
+            requests.post('http://n64storageflask-env.elasticbeanstalk.com/sessions',
+                    data=query_string, headers={'Content-Type': 'application/json'})
+        return render(request, 'query.html', {'form': form, 'result': result})
+
     else:
         form = QueryForm()
     return render(request, 'query.html', {'form': form})
 
 def watch(request):
-    return render(request, 'watch.html')
+    if request.method == 'get':
+        form = WatchForm(request.GET)
+        if form.is_valid():
+            cd = form.cleaned_data
+            ##ask for your video  
+            ##video_num = cd['video']
+            ##video = json.dumps({'video': video_num})
+            ##requests.post('http://n64storageflask-env.elasticbeanstalk.com/sessions',
+            ##        data=video, headers={'Content-Type': 'application/json'})
+            ##return render(request, 'watch.html', {'form': form, 'video': video})
+    else:
+        return render(request, 'watch.html', {'form': form})
 
 def upload(request):
+    if request.method == 'POST':
+        url = request.POST['video_url']
+        session_data = json.dumps({'video_url': url})
+        requests.post('http://n64storageflask-env.elasticbeanstalk.com/sessions',
+                data=session_data, headers={'Content-Type': 'application/json'})
+        return redirect('upload')
+
     return render(request, 'upload.html')
-
-def login(request):
-    return render(request, 'base.html')
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    return render(request, 'current_datetime.html', {'current_date': now})
-
-def hours_ahead(request, offset):
-    try:
-        offset = int(offset)
-    except ValueError:
-        raise Http404()
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
