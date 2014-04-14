9f980f2ca902c262eb5d14df6e8d9f2d29983e0d
Jonathan Bell <jbell@bu.edu>
2014-04-14 14:33:34 -0400

    added requirements.txt, changed base and query

--- n64/forms.py
+++ n64/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 
 class QueryForm(forms.Form):
-    event = forms.CharField(required=False)
-    race = forms.CharField(required=False)
-    player = forms.CharField(required=False)
+    Elements = forms.CharField(required=False)
+    Conditions = forms.CharField(required=False)
 
-class QueryForm(forms.Form):
+class VideoForm(forms.Form):
     event = forms.CharField(required=False)
     race = forms.CharField(required=False)
     player = forms.CharField(required=False)
