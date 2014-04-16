834391edac2bb59ba59a4dbb10a3385a678f9e10
Jonathan Bell <jbell@bu.edu>
2014-04-15 21:56:48 -0400

    added n64/views.py

--- n64/views.py
+++ n64/views.py
@@ -56,3 +56,27 @@ def upload(request):
         return redirect('upload')
 
     return render(request, 'upload.html')
+
+def sign_request(request):
+    AWS_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET = os.environ['AWS_SECRET_KEY']
+    S3_BUCKET = 'session-videos'
+
+    uid = uuid.uuid1()
+    ext = request.GET['s3_object_name'].split('.')[-1]
+    object_name = 'raw/' + str(uid) + '.' + ext
+    mime_type = request.GET['s3_object_type']
+    expires = int(time.time() + 10)
+    amz_headers = "x-amz-acl:public-read"
+    put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+
+    signature = base64.encodestring(hmac.new(AWS_SECRET, put_request, hashlib.sha1).digest())
+    signature = urllib.quote_plus(signature.strip())
+
+    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+    resp = json.dumps({
+        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ID, expires, signature),
+        'url': url
+    })
+    return HttpResponse(resp, content_type='text/plain; charset=x-user-defined')
+
