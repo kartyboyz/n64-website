76a8a52bdbcf377bd34387428acc9551b71c189e
Jonathan Bell <jbell@bu.edu>
2014-04-15 23:04:37 -0400

    added naming for redirects in urls.py

--- n64/urls.py
+++ n64/urls.py
@@ -1,22 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from n64.forms import QueryForm
-from n64.views import home, query, watch, upload, sign_request
 from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-              url(r'^$', home),
-              url(r'^home/$', home),
-			  url(r'^query/$', query),
-			  url(r'^watch/$', watch),
-			  url(r'^upload/$', upload),
-			  url(r'^sign_s3/$', sign_request),
-			  url(r'^login/$', login),
-			  url(r'^logout/$', logout),
+urlpatterns = patterns('n64.views',
+              url(r'^$', name='home'),
+              url(r'^home/$', name='home'),
+			  url(r'^query/$', name='query'),
+			  url(r'^watch/$', name='watch'),
+			  url(r'^upload/$', name='upload'),
+			  url(r'^sign_s3/$', name='sign_request'),
+			  url(r'^login/$', name='login'),
+			  url(r'^logout/$', name='logout'),
 )
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
