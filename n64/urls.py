85c536c2346a2651935139d2d3c6f145743de026
Jonathan Bell <jbell@bu.edu>
2014-04-16 11:51:07 -0400

    changed urls.py logout config

--- n64/urls.py
+++ n64/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('n64.views',
 
 urlpatterns += patterns('',
 			  url(r'^login/$', login),
-			  url(r'^logout/$', logout, next_page='n64.views.home'),
+			  url(r'^logout/$', logout, 'n64.views.home'),
 )
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
